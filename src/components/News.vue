7c65d501d33d5e068f525d8b7d954c962e9512b1
Veli <vdonmez42@gmail.com>
2019-03-07 17:46:09 +0300

    first day

---
+++ src/components/News.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="news">
+    <ul class="news-list">
+      <li v-for="item in news" :key="item.id" class="news-item">
+        <span class="score">{{item.score}}</span>
+        <span class="title">{{item.title}}</span>
+        <br>
+        <span class="meta">
+          <span class="by">
+            by
+            {{item.by}} | 
+          </span>
+          <span class="time">{{item.time}} | </span>
+        </span>
+        <span class="label">{{item.type}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import news from "@/assets/news.json";
+
+export default {
+  data() {
+    return {
+      news: news
+    };
+  },
+  name: "News",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.news-list {
+  position: absolute;
+  margin: 30px 0;
+  width: 100%;
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+.news-list {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+.news-item {
+  background-color: #fff;
+  padding: 20px 30px 20px 80px;
+  border-bottom: 1px solid #eee;
+  position: relative;
+  line-height: 20px;
+}
+.news-item .score {
+  color: #f60;
+  font-size: 1.1em;
+  font-weight: 700;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 80px;
+  text-align: center;
+  margin-top: -10px;
+}
+.news-item .meta,
+.news-item .host {
+  font-size: 0.85em;
+  color: #828282;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Navbar/>
+    <News></News>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import News from "@/components/News.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    News,
+    Navbar
   }
 };
 </script>
